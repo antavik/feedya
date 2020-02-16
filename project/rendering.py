a3a7358c3114f996279a8c6f09b9b1b5e7ba624d
Anton <41118912+anton-svechnikau@users.noreply.github.com>
2020-02-16 16:37:09 +0300

    Add randomizer (#4)

--- project/rendering.py
+++ project/rendering.py
@@ -1,6 +1,7 @@
 import logging
 
 from typing import Tuple
+from random import choice
 
 from jinja2 import Environment, FileSystemLoader
 from jinja2.environment import Template
@@ -8,6 +9,23 @@ from jinja2.environment import Template
 from settings import TEMPLATES_DIR, TEMPLATE_NAME, TODAY_DATETIME
 from entities import FeedEntity
 from feeds import FEEDS
+
+
+def color_randomizer() -> Tuple[str]:
+    _support_color_pairs = (
+        ('#fff3b0', '#3574cc',),
+        ('#25b8a9', '#b82534',),
+        ('#b6ff1c', '#651cff',),
+        ('#19f76d', '#f719a3',),
+        ('#aec575', '#8c75c5',),
+        ('#8dcf83', '#c583cf',),
+        ('#6dd81b', '#861bd8',),
+        ('#96ca98', '#ca96c8',),
+        ('#2a7e6c', '#7e2a3c',),
+        ('#42e4e7', '#e74542',),
+    )
+
+    return choice(_support_color_pairs)
 
 
 def _get_html_template() -> Template:
@@ -22,7 +40,11 @@ def _get_html_template() -> Template:
 def render_html_email(feeds: Tuple[FeedEntity]) -> str:
     template = _get_html_template()
 
+    title_bg_color, title_font_color = color_randomizer()
+
     email = template.render(
+        title_bg_color=title_bg_color,
+        title_font_color=title_font_color,
         feed_datetime=TODAY_DATETIME,
         feeds=FEEDS
     )
