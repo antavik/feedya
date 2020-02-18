e345edb1e5c80eac0ab34474abce778a8425650e
anton-svechnikau <anton.svechnikau@gmail.com>
2020-02-18 09:33:28 +0300

    Update approach to work with DB

--- project/database.py
+++ project/database.py
@@ -1,53 +1,70 @@
 import logging
 import contextlib
+import json
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (
+    create_engine, Table, Column, Integer, String, DateTime, MetaData,
+)
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.exc import IntegrityError
 
 from settings import DB_URL
-from models.model import Model
-from models import News
 from entities import NewsItemEntity
 from exceptions import NewsAlreadyExists
 
 
 _ENGINE = create_engine(DB_URL)
 
-Model.metadata.create_all(_ENGINE)
+_METADATA = MetaData()
 
-_SESSION = sessionmaker(bind=_ENGINE)
+news = Table(
+    'news',
+    _METADATA,
+    Column('id', Integer, primary_key=True),
+    Column('feed', String(50), nullable=False),
+    Column('title', String(200), nullable=False),
+    Column('url', String(300), nullable=False, unique=True, index=True),
+    Column('publication_date', DateTime, nullable=False),
+    Column('collection_date', DateTime, nullable=False),
+    Column('data', JSONB)
+)
+
+_METADATA.create_all(_ENGINE)
 
 
 @contextlib.contextmanager
-def establish_session():
+def establish_connection():
     try:
-        session = _SESSION()
+        connection = _ENGINE.connect()
     except Exception as exp:
         logging.error(exp)
     else:
-        yield session
+        yield connection
     finally:
-        session.close()
+        connection.close()
 
 
 def create_news(entity: NewsItemEntity) -> NewsItemEntity:
-    obj = News(feed=entity.feed,
-               title=entity.title,
-               url=entity.url,
-               publication_date=entity.publication_date,
-               collection_date=entity.collection_date,
-               data=entity.data)
+    query = news.insert().values(
+        feed=entity.feed,
+        title=entity.title,
+        url=entity.url,
+        publication_date=entity.publication_date,
+        collection_date=entity.collection_date,
+        data=json.dumps(entity.data)
+    )
+    query.compile()
 
-    with establish_session() as session:
+    with establish_connection() as connection:
         try:
-            session.add(obj)
-            session.commit()
-        except IntegrityError:
-            session.rollback()
+            result = connection.execute(
+                query
+            )
 
+            query.bind = _ENGINE
+        except IntegrityError:
             raise NewsAlreadyExists(f'News {entity.url} already exists.')
         else:
-            entity.pk = obj.id
+            entity.pk = result.inserted_primary_key
 
     return entity
